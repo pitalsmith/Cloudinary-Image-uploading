c89c8694db6d049e221bf38072de31517670df4f
pitalsmith <atundepeter@gmail.com>
2023-02-22 13:10:14 +0100

    first commit

---
+++ server/uploadImage.js
@@ -0,0 +1,64 @@
+var cloudinary = require("cloudinary").v2;
+require('dotenv').config()
+
+// const cloud_name = "dnb0b5bqg";
+// const api_key = "446447357288346";
+// const api_secret = "0mGlOeS1Fs7yuaxJTCVfHUHJfYY";
+
+// cloudinary.config({
+//   cloud_name: process.env.CLOUD_NAME,
+//   api_key: process.env.API_KEY,
+//   api_secret: process.env.API_SECRET,
+// });
+
+const cloud_name =  process.env.CLOUD_NAME;
+const api_key = process.env.API_KEY;
+const api_secret = process.env.API_SECRET;
+
+cloudinary.config({
+  cloud_name: cloud_name,
+  api_key: api_key,
+  api_secret: api_secret,
+});
+
+const opts = {
+  overwrite: true,
+  invalidate: true,
+  resource_type: "auto",
+};
+
+const uploadImage = (image) => {
+  //imgage = > base64
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(image, opts, (error, result) => {
+      if (result && result.secure_url) {
+        console.log(result.secure_url);
+        return resolve(result.secure_url);
+      }
+      console.log(error.message);
+      return reject({ message: error.message });
+    });
+  });
+};
+module.exports = (image) => {
+  //imgage = > base64
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(image, opts, (error, result) => {
+      if (result && result.secure_url) {
+        console.log(result.secure_url);
+        return resolve(result.secure_url);
+      }
+      console.log(error.message);
+      return reject({ message: error.message });
+    });
+  });
+};
+
+module.exports.uploadMultipleImages = (images) => {
+  return new Promise((resolve, reject) => {
+    const uploads = images.map((base) => uploadImage(base));
+    Promise.all(uploads)
+      .then((values) => resolve(values))
+      .catch((err) => reject(err));
+  });
+};
